1a056659834f20583697a18d31518f8dae5def70
Milan Felix Šulc <rkfelix@gmail.com>
2023-07-27 08:39:48 +0200

    Tests: replace ninjify to contributte

--- tests/Cases/DI/Psr6CachingExtensionTest.php
+++ tests/Cases/DI/Psr6CachingExtensionTest.php
@@ -5,29 +5,23 @@ namespace Tests\Contributte\Psr6\Unit\DI;
 use Contributte\Psr6\CachePoolFactory;
 use Contributte\Psr6\DI\Psr6CachingExtension;
 use Contributte\Psr6\ICachePoolFactory;
+use Contributte\Tester\Environment;
+use Contributte\Tester\Toolkit;
+use Contributte\Tester\Utils\ContainerBuilder;
 use Nette\Bridges\CacheDI\CacheExtension;
 use Nette\DI\Compiler;
-use Nette\DI\Container;
-use Nette\DI\ContainerLoader;
-use Ninjify\Nunjuck\Toolkit;
 use Tester\Assert;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
-function createContainer(): Container
-{
-	$tempDir = TEMP_DIR;
-	$loader = new ContainerLoader($tempDir, true);
-	$class = $loader->load(static function (Compiler $compiler) use ($tempDir): void {
-		$compiler->addExtension('cache', new CacheExtension($tempDir));
-		$compiler->addExtension('psr6', new Psr6CachingExtension());
-	}, random_bytes(10));
-
-	return new $class();
-}
-
 Toolkit::test(function (): void {
-	$container = createContainer();
+	$container = ContainerBuilder::of()
+		->withCompiler(static function (Compiler $compiler): void {
+			$compiler->addExtension('cache', new CacheExtension(Environment::getTestDir()));
+			$compiler->addExtension('psr6', new Psr6CachingExtension());
+		})
+		->build();
+
 	Assert::true($container->hasService('psr6.factory'));
 	Assert::type(CachePoolFactory::class, $container->getService('psr6.factory'));
 	Assert::type(CachePoolFactory::class, $container->getByType(ICachePoolFactory::class));
